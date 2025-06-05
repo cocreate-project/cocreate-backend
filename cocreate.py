68585df034065eeb519bf1c5735e7c65216e4396
martinetbn <martinetchebarne829@gmail.com>
2025-06-05 14:07:03 -0300

    Add CORS support to the application and update requirements

--- cocreate.py
+++ cocreate.py
@@ -1,10 +1,12 @@
 from dotenv import load_dotenv
 from cocreate import create_app
 from cocreate.utils import db
+from flask_cors import CORS
 
 load_dotenv()
 
 app = create_app()
+CORS(app)
 
 if __name__ == "__main__":
     db.create_database()
